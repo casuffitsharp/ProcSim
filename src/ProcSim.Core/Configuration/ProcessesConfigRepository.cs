b0fde3714ddc049d3b23d083387dbe7eafda3c75
Henrique Feliciano de Azevedo <hfazevedo@furb.br>
2025-06-22 21:23:45 -0300

    changed file extensions

--- src/ProcSim.Core/Configuration/ProcessesConfigRepository.cs
+++ src/ProcSim.Core/Configuration/ProcessesConfigRepository.cs
@@ -2,6 +2,6 @@
 
 public class ProcessesConfigRepository : RepositoryBase<List<ProcessConfigModel>>
 {
-    public override string FileExtension => ".pspconfig";
+    public override string FileExtension => ".pconfig";
     public override string FileFilter => $"Process Config Files (*{FileExtension})|*{FileExtension}";
 }
--- src/ProcSim.Core/Configuration/VmConfigRepository.cs
+++ src/ProcSim.Core/Configuration/VmConfigRepository.cs
@@ -2,6 +2,6 @@
 
 public class VmConfigRepository : RepositoryBase<VmConfigModel>
 {
-    public override string FileExtension => ".psvmconfig";
+    public override string FileExtension => ".vmconfig";
     public override string FileFilter => $"VM Config Files (*{FileExtension})|*{FileExtension}";
 }
