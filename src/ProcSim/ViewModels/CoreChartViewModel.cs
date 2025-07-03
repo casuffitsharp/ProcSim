497882177b08032f396567213c1747c3a8dcd34e
Henrique Feliciano de Azevedo <hfazevedo@furb.br>
2025-07-02 23:40:35 -0300

    fix division by zero in cpu metrics

--- src/ProcSim/ViewModels/CoreChartViewModel.cs
+++ src/ProcSim/ViewModels/CoreChartViewModel.cs
@@ -10,8 +10,13 @@ public class CoreChartViewModel(string name) : ChartViewModelBase(60, name)
 
     public void AddValue(CpuUsageMetric metric)
     {
-        double systemUsage = (metric.SyscallCyclesDelta + metric.InterruptCyclesDelta) * 100.0 / metric.CyclesDelta;
-        double userUsage = metric.UserCyclesDelta * 100.0 / metric.CyclesDelta;
+        double systemUsage = 0.0;
+        double userUsage = 0.0;
+        if (metric.CyclesDelta > 0)
+        {
+            systemUsage = Math.Min((metric.SyscallCyclesDelta + metric.InterruptCyclesDelta) * 100.0 / metric.CyclesDelta, 100);
+            userUsage = Math.Min(metric.UserCyclesDelta * 100.0 / metric.CyclesDelta, 100);
+        }
 
         AddSeries(USER_SERIES_ID, "User", SKColors.Blue);
         AddValue(USER_SERIES_ID, userUsage);
